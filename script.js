b4694c972eb0fd260ecca0d837586c6289dd4661
Sergio <sergioparilli06@gmail.com>
2024-12-06 20:40:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector (".container");
-const btnSignIn = document.getElementById("btn-sign-in");
-const btnSignUp = document.getElementById("btn-sign-up");
+const btnSignIn = document.getElementById ("btn-sign-in");
+const btnSignUp = document.getElementById ("btn-sign-up");
 
 btnSignIn.addEventListener("click",()=>{
     container.classList.remove("toggle");
